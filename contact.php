4d5d10f0d266181e364c24928145e2e0941a4c5b
doublemario <doublemario44@gmai.com>
2018-03-19 15:37:47 +0100

    ok1

--- contact.php
+++ contact.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Test GIT 2</title>
+	<title>Test GIT 1</title>
 </head>
 <body>
 <h1>Page d'accueil</h1>
 <h2> Le temps c'est l'argent</h2>
-<p> du texte</p>
 
+<p> du texte</p>
 </body>
 </html>
